7c1b7facd3ff93539c3b6e0dea88615a773e2910
Maximiliam Karlsson <shrinkingcash@hotmail.com>
2014-06-02 14:34:38 +0200

    Added a hash with romkeys.

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -1,6 +1,7 @@
 def romanize(input)
   raise ArgumentError, 'can not encode negative number' if input < 0
   raise ArgumentError, 'can not encode zero' if input == 0
+  romkeys = {1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV' ,1 => 'I'}
   output = ' '
   return output
 end
