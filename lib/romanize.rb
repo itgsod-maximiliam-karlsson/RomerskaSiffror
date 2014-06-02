40ec76ca04071782b16d00076beb589587b9f349
Maximiliam Karlsson <shrinkingcash@hotmail.com>
2014-06-02 14:38:06 +0200

    Added a loop until input == 0

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -3,5 +3,8 @@ def romanize(input)
   raise ArgumentError, 'can not encode zero' if input == 0
   romkeys = {1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV' ,1 => 'I'}
   output = ' '
+  until input == 0
+    
+  end
   return output
 end
